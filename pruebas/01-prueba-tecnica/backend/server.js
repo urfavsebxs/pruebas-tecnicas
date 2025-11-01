d2b617ce5b37bc6b592a8993073abace2472adac
Sebxs940 <sprogramacion940@gmail.com>
2025-11-01 10:25:54 -0500

    Refactor product API routes: remove individual product endpoint handlers and consolidate under productRoutes

--- pruebas/01-prueba-tecnica/backend/server.js
+++ pruebas/01-prueba-tecnica/backend/server.js
@@ -1,17 +1,12 @@
 import express from "express";
-import { postProducts } from "./api/postProducts.js";
-import { deleteProducts } from "./api/deleteProducts.js";
-import { getProducts } from "./api/getProducts.js";
-import { putProducts } from "./api/putProducts.js";
+// import * as Products from "./api/products.js";
+import productRoutes from "./api/routes/productRoutes.ts";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/postProducts", postProducts);
-app.put("/api/putProducts/:id", putProducts);
-app.delete("/api/deleteProducts", deleteProducts);
-app.get("/api/getProducts", getProducts);
+app.use("/api/products", productRoutes);
 
 app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
